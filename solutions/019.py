aa3f18144967bd9296e8c0d9915314821cd4cfa7
hoanvu <vumaihoan@gmail.com>
2015-10-23 14:50:02 +0700

    Add comment to 019.py

--- solutions/019.py
+++ solutions/019.py
@@ -4,9 +4,14 @@
 
 
 def convert(string):
+    # Split chuỗi vừa nhập sang một mảng
     words = string.split(' ')
+
+    # Với mỗi từ là thành phần của mảng, convert chữ cái đầu tiên sang in hoa
+    # Các từ còn lại chuyển sang viết thường.
     new_words = [word[0].upper() + word[1:].lower() for word in words]
 
+    # Join các từ vừa được convert thành chuỗi
     return ' '.join(new_words)
 
 
